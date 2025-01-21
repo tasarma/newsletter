0996a8f4ccb424eac38b46f90d07aee0ab04ffa0
tasarma <tasarma@itu.edu.tr>
2025-01-21 11:33:59 +0100

    end of chapter 5

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
 use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
 use newsletter::telemetry::{get_subscriber, init_subscriber};
-use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let subcriber = get_subscriber("newsletter".into(), "info".into(), std::io::stdout);
-    init_subscriber(subcriber);
-    let configuration = get_configuration().expect("Failed to read configuration");
+    let subscriber = get_subscriber("newsletter".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
+    let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool =
-        PgPool::connect(&configuration.database.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+        PgPoolOptions::new().connect_lazy_with(configuration.database.connect_options());
+
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,13 +1,13 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
-use actix_web::middleware::Logger;
+use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
-    let db_pool = web::Data::new(db_pool);
+    let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
@@ -17,6 +17,5 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     })
     .listen(listener)?
     .run();
-
     Ok(server)
 }
