f29e6a149ae24841c4278a8ea7ec443ab3c62243
tasarma <tasarm7@protonmail.com>
2025-09-21 14:45:46 +0200

    3.5 -> Run our application as a background task with tokio::spawn

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,21 @@
-use newsletter::main;
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    spawn_app();
+    let client = reqwest::Client::new();
 
-#[test]
-fn dummy() {
-    main()
+    // Act
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    todo!()
 }
