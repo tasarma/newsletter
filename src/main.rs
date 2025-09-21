dc2f75ab8d0bd07228cd993a7704f3e4988c1792
tasarma <tasarm7@protonmail.com>
2025-09-21 15:18:33 +0200

    3.5 -> Assign random port from TcpListener for tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::net::TcpListener;
+
 use newsletter::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
