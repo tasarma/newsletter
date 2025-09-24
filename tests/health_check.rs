ebb17324afe88bfc253df91929dba1eb8f1937e4
tasarma <tasarm7@protonmail.com>
2025-09-24 13:56:45 +0200

    3.8.5.3 Connecting To Postgres

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,3 +1,5 @@
+use newsletter::configuration::get_configuration;
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 
 fn spawn_app() -> String {
@@ -31,6 +33,12 @@ async fn health_check_works() {
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres");
+
     let client = reqwest::Client::new();
 
     // Act
