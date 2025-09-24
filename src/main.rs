cee766c03edf665248634cc0ac19aaafe01005b8
tasarma <tasarm7@protonmail.com>
2025-09-24 13:44:17 +0200

    3.8.5.2.2 Reading A Configuration File

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::net::TcpListener;
 
+use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let address = TcpListener::bind("127.0.0.1:8000")?;
-    run(address)?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
