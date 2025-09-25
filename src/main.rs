bdf54266921a3bb14211a7bf8a1d6be4ea6cd10d
tasarma <tasarm7@protonmail.com>
2025-09-25 09:32:33 +0200

    3.9.1 Application State In actix-web, added postgres connection to app state

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ use std::net::TcpListener;
 
 use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
+use sqlx::{Connection, PgConnection};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,18 @@
 use std::net::TcpListener;
 
-use actix_web::{App, HttpServer, dev::Server, web};
+use actix_web::{dev::Server, web, App, HttpServer};
+use sqlx::PgConnection;
 
 use crate::routes::{health_check, subscribe};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    let connection = web::Data::new(connection);
+
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(web::Data::new(connection.clone()))
     })
     .listen(listener)?
     .run();
