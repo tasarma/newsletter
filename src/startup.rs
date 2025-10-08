835b36f5c8e84838404f20f5ca479beff0ab79e9
tasarma <tasarm7@protonmail.com>
2025-10-08 23:42:31 +0200

    4.3.2 -> actix-web’s Logger Middleware

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
 
-use actix_web::{App, HttpServer, dev::Server, web};
+use actix_web::{App, HttpServer, dev::Server, middleware::Logger, web};
 use sqlx::PgPool;
 
 use crate::routes::{health_check, subscribe};
@@ -10,6 +10,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
 
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
