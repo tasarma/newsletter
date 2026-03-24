1c4c6081e421da11f5275d7060a9fd8ddc505b4c
tasarma <tasarm7@protonmail.com>
2026-03-24 10:23:49 +0100

    added validator crate and domain tests are green

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -1,10 +1,15 @@
+use validator::ValidateEmail;
+
 #[derive(Debug)]
 pub struct SubscriberEmail(String);
 
 impl SubscriberEmail {
     fn parse(s: String) -> Result<SubscriberEmail, String> {
-        // TODO: add validation!
-        Ok(Self(s))
+        if s.validate_email() {
+            Ok(Self(s))
+        } else {
+            Err(format!("{} is not a valid subscriber email", s))
+        }
     }
 }
 
