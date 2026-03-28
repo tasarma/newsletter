b871ed1690b798f54c12c400f2dc6fed84fd3554
tasarma <tasarm7@protonmail.com>
2026-03-28 10:17:54 +0100

    7.2.2.1 reqwest::Client

--- src/email_client.rs
+++ src/email_client.rs
@@ -1,11 +1,23 @@
+use reqwest::Client;
+
 use crate::domain::SubscriberEmail;
 
 #[derive(Debug)]
 pub struct EmailClient {
     sender: SubscriberEmail,
+    http_client: Client,
+    base_url: String,
 }
 
 impl EmailClient {
+    pub fn new(base_url: String, sender: SubscriberEmail) -> Self {
+        Self {
+            sender,
+            http_client: Client::new(),
+            base_url,
+        }
+    }
+
     pub async fn send_email(
         &self,
         recipient: SubscriberEmail,
