7a5a3596db590f289bcfa9cc949d08bb39e019c5
tasarma <tasarm7@protonmail.com>
2026-04-03 17:03:05 +0200

    7.7.1.1 Red test: modified TestApp for email_server

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -2,6 +2,7 @@ use secrecy::Secret;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::sync::LazyLock;
 use uuid::Uuid;
+use wiremock::MockServer;
 
 use newsletter::configuration::{DatabaseSettings, get_configuration};
 use newsletter::startup::{Application, get_connection_pool};
@@ -24,6 +25,7 @@ static TRACING: LazyLock<()> = LazyLock::new(|| {
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
+    pub email_server: MockServer,
 }
 
 impl TestApp {
@@ -43,10 +45,13 @@ pub async fn spawn_app() -> TestApp {
     // All other invocations will instead skip execution.
     LazyLock::force(&TRACING);
 
+    let email_server = MockServer::start().await;
+
     let configuration = {
         let mut c = get_configuration().expect("Failed to read configuration.");
         c.database.database_name = Uuid::new_v4().to_string();
         c.application.port = 0;
+        c.email_client.base_url = email_server.uri();
         c
     };
 
@@ -61,6 +66,7 @@ pub async fn spawn_app() -> TestApp {
     TestApp {
         address,
         db_pool: get_connection_pool(&configuration.database),
+        email_server,
     }
 }
 
--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -1,3 +1,8 @@
+use wiremock::{
+    Mock, ResponseTemplate,
+    matchers::{method, path},
+};
+
 use crate::helpers::spawn_app;
 
 #[tokio::test]
@@ -69,4 +74,24 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
             description
         );
     }
+}
+
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_for_valid_data() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    // Act
+    app.post_subscriptions(body.into()).await;
+
+    // Assert
+    // Mock asserts on drop
 }
